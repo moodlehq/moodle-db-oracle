8c48650b4ad4a12004037626a9126f306a782fa6
Eloy Lafuente (stronk7) <stronk7@moodle.org>
2022-06-10 01:05:16 +0200

    Switch back to XE (CBD), don't use PDB at all

--- root/container-entrypoint-initdb.d/01-mdb.sql
+++ root/container-entrypoint-initdb.d/01-mdb.sql
@@ -1,21 +1,13 @@
 -- Not needed to connect, the container does it for us.
 -- conn sys/oracle as sysdba;
 
--- Create our MDB pluggable database into MDB directory, with moodle database user.
-CREATE PLUGGABLE DATABASE MDB
-    ADMIN USER moodle IDENTIFIED BY "m@0dl3ing"
-        ROLES=(CONNECT, RESOURCE, DBA)
-        DEFAULT TABLESPACE users
-            DATAFILE '/opt/oracle/mdb/users01.dbf' SIZE 384M AUTOEXTEND ON MAXSIZE UNLIMITED
-    PATH_PREFIX = '/opt/oracle/mdb'
-    FILE_NAME_CONVERT = (
-        '/opt/oracle/oradata/XE/pdbseed',
-        '/opt/oracle/mdb')
-    NOLOGGING;
+ALTER PROFILE DEFAULT LIMIT PASSWORD_LIFE_TIME UNLIMITED;
+CREATE USER moodle IDENTIFIED BY "m@0dl3ing";
 
--- Everything is done, open the database.
-ALTER PLUGGABLE DATABASE MDB OPEN;
-ALTER PLUGGABLE DATABASE MDB SAVE STATE;
+GRANT CONNECT,RESOURCE,DBA TO moodle;
+GRANT CREATE SESSION TO moodle WITH ADMIN OPTION;
+GRANT UNLIMITED TABLESPACE TO moodle;
+GRANT EXECUTE ON DBMS_LOCK to moodle;
 
 -- Delete the default pluggable database, we don't use it (that's 600MB). Not required, but better off.
 ALTER PLUGGABLE DATABASE XEPDB1 CLOSE;
@@ -26,28 +18,7 @@ ALTER SESSION SET "_oracle_script" = TRUE;
 ALTER PLUGGABLE DATABASE PDB$SEED CLOSE;
 DROP PLUGGABLE DATABASE PDB$SEED INCLUDING DATAFILES;
 
--- Change to the pluggable database (MDB) so we split from XE.
-ALTER SESSION SET CONTAINER = MDB;
-
---- Size some of the tablespaces to avoid too much extending.
-ALTER DATABASE DATAFILE '/opt/oracle/mdb/system01.dbf' RESIZE 384M;
-ALTER DATABASE DATAFILE '/opt/oracle/mdb/undotbs01.dbf' RESIZE 64M;
-ALTER DATABASE TEMPFILE '/opt/oracle/mdb/temp01.dbf' RESIZE 64M;
-
--- Tidy moodle permisions.
-ALTER PROFILE DEFAULT LIMIT PASSWORD_LIFE_TIME UNLIMITED;
-GRANT CREATE SESSION TO moodle WITH ADMIN OPTION;
-GRANT UNLIMITED TABLESPACE TO moodle;
-GRANT EXECUTE ON DBMS_LOCK to moodle;
-
--- Shutdown the pluggable database for the changes above to take effect.
-SHUTDOWN IMMEDIATE;
-STARTUP;
-
 -- System settings.
-
--- Disabled for now, some bug makes mdb files to be lost if we restart the CBD.
-ALTER SESSION SET CONTAINER = CDB$ROOT;
 
 -- Disable ASYNC IO unconditionally. Really makes a difference.
 ALTER SYSTEM SET FILESYSTEMIO_OPTIONS = DIRECTIO SCOPE = SPFILE;
